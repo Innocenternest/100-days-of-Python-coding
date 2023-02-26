0c40cdbe4cfba83322e2dbc9a836fee3e0a704ab
innocenternest <innocenternest0808@gmail.com>
2023-02-26 06:46:45 -0800

    Leap Year Calculator

---
+++ Day3_project/day3.2_project.py
@@ -0,0 +1,24 @@
+#This is how you work out whether if a particular year is a leap year.
+
+#on every year that is evenly divisible by 4 
+
+#except** every year that is evenly divisible by 100 
+
+#unless** the year is also evenly divisible by 400
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Refer to the flow chart here: https://bit.ly/36BjS2D
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
